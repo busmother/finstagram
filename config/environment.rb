7334c127ae96c1f48a910c5e03eb027d5ea8ab3d
Ian Marshall <imarshall14@gmail.com>
2021-08-04 14:47:47 -0400

    create Posts controller with #create action and post_params

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Store uploaded files on the local file system
+# config.active_storage.service = :local
