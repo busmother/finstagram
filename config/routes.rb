3ac44832207b4f1bba30505ec1cdd5c54411e0b5
Ian Marshall <imarshall14@gmail.com>
2021-08-06 15:20:42 -0400

    update devise_for :users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'search/index'
-  devise_for :users
+  # devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'home/index'
   root to: 'home#index'
   resources :users, only: [:show, :edit, :update]
