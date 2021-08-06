b84c80ecd49e384b51a343f833c0c8d4af5645ff
Ian Marshall <imarshall14@gmail.com>
2021-08-06 15:00:41 -0400

    create current_user aliasing in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    def devise_current_user
+        @devise_current_user ||= warden.authenticate(scope: :user)
+      end
+      
+    def current_user
+        if params[:user_id].blank?
+            devise_current_user
+        else
+            User.find(params[:user_id])
+        end   
+    end
+
 end
